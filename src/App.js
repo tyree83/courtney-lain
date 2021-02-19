d186f407de54a97ccedd9bce6fa6c923b2a68630
Tyree Christensen <tyree83@gmail.com>
2021-02-19 08:45:19 -0500

    Navbar Completed

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css"
+import Navbar from './components/Navbar'
 
 function App() {
   return (
-  <h2>Courtney Lain - Freelance Author</h2>
+  <Navbar />
   );
 }
 
